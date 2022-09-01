285c658f3cedfb375534a2308f2520cc117d729c
adityayadav12345 <112612965+adityayadav12345@users.noreply.github.com>
2022-09-01 12:55:33 +0530

    Create diamond.java

---
+++ diamond.java
@@ -0,0 +1,31 @@
+class Main {
+    public static void main(String[] args) {
+     
+      for(int i = 1;i<=6;i++){
+          for(int j =1; j<=6-i;j++){
+               System.out.print(" ");
+          }
+           for (int j = 1;j<=i;j++){
+               System.out.print("*");
+           }
+           for(int j =1 ;j<=i;j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+       for (int i = 1;i<=5;i++){
+           for(int j= 1;j<=i;j++){
+               System.out.print(" ");
+           }
+           for( int j = 5;j>=i;j--){
+               System.out.print("*");
+           }
+           for(int j =5 ; j>=i;j--){
+               System.out.print("*");
+           }
+           
+           System.out.println(" ");
+    }
+
+    }
+}
